02112252ae4346dd389c3105c4f9626e26459715
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2025-02-25 13:14:45 +0100

    Update test.sol

--- contracts/testpool/test.sol
+++ contracts/testpool/test.sol
@@ -1,1 +1,23 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "node_modules/@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "node_modules/@uniswap/v3-core/contracts/UniswapV3Factory.sol";
+import "node_modules/@uniswap/v3-periphery/contracts/NonfungiblePositionManager.sol";
+
+contract Token2 is ERC20, UniswapV3Factory, NonfungiblePositionManager {
+
+    address stabletoken, pool;
+    constructor(address _stabletoken) ERC20("USDC", "USDC") {
+        _mint(msg.sender, 100 * 10 ** 18);
+        stabletoken = _stabletoken;
+    }
+
+    function createpoolwithliquidity() public{
+        pool = createPool(address(this), stabletoken, 3000);
+        mint(pool, 1000000000000000000, 1000000000000000000, "");
+    }
+}
